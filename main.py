5e7da9035d946027ed7005d9f7354c62e7632458
Pratheek Nayak B K <128373632+NayakPratheek@users.noreply.github.com>
2024-06-20 18:15:45 +0530

    gemini ai integration

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import cv2
 import numpy as np
 from cvzone.HandTrackingModule import HandDetector
+import google.generativeai as genai
+from PIL import Image
+
+# API key and api model
+genai.configure(api_key="AIzaSyDIvahoFu6sUfFJc7I4z7VLD9-GSUECZ3M")
+model = genai.GenerativeModel('gemini-1.5-flash')
+
 
 # Initialize the webcam to capture video
 cap = cv2.VideoCapture(0)
@@ -26,13 +33,22 @@ def draw(canvas, info, prev_pos):
     fingers, lmList = info
     current_position = None
 
-    if fingers == [0, 1, 0, 0, 0]:
+    if fingers == [0, 1, 0, 0, 0]:  #finger position (only when index finger is up)
         current_position = lmList[8][0:2]  # Index finger tip positioning
         if prev_pos is not None:
-            cv2.line(canvas, prev_pos, current_position, (255, 0, 255), 10)  # Drawing line
+            cv2.line(canvas, prev_pos, current_position, (255, 255, 0), 10)  # Drawing line
             prev_pos = current_position
 
     return current_position
+
+
+# Sending request to Gemini AI
+def sendToAi(model,canvas, fingers):
+    if fingers == [1,1,0,0,1]:          #fingers position(swag gesture)
+        pil_image = Image.fromarray(canvas)                              #taking the image from the canvas and converting to text
+        response = model.generate_content(["Solve",pil_image])           #sending the converted text to ai
+        print(response.text)
+
 
 prev_pos = None
 canvas = None
@@ -47,10 +63,12 @@ while True:
         canvas = np.zeros_like(img)  # Initialize canvas with same size as img and setting its background to black
 
     info = getHandInfo(img)
+
     if info:
         fingers, lmList = info
-        print(fingers)
+        # print(fingers)
         prev_pos = draw(canvas, info, prev_pos)
+        sendToAi(model,canvas,fingers)
 
     # Combine the original image with the canvas for display
     combined_img = cv2.addWeighted(img, 0.7, canvas, 0.3, 0)
